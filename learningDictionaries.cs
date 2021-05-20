94932d461e2be2f08b9222ce1207d7e37b713139
ataahOnGithub <69350396+ataahOnGithub@users.noreply.github.com>
2021-05-20 19:03:52 -0400

    week1Assignments
    Assignments for week 1 (.Net Course)

---
+++ learningDictionaries.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+/* Creating a dictionary
+ * Following instructors example
+ */
+
+namespace learningDictionaries
+{
+
+    // Classes can have state and behavior (added to Methods)
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            // Instance creating a dictionary
+            Dictionary<int, string> dictionary = new Dictionary<int, string>
+            {
+
+                // Adding entries to the dictionary
+                { 101, "Robert" },
+                { 102, "Michael" },
+
+                { 103, "Roxie" },
+                { 104, "Shelly" }
+            };
+
+            // Changing values already in the dictionary
+            dictionary[101] = "Rachel";
+            dictionary[102] = "Michelle";
+
+            // Allow user to input data into the dictionary
+            Console.WriteLine("Your user ID is 105" +
+                "\nWhen prompted, please enter it below.");
+            Console.WriteLine("Please enter your User ID: ");
+            int newKey = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Welcome to class, " +
+                "please enter your first name to continue: ");
+            string newName;
+            newName = Console.ReadLine();
+            Console.WriteLine("Hello {0} welcome. " +
+                "Confirm that you've been added to the chart below " +
+                "and type 'ok'", newName);
+            dictionary.Add(newKey, newName);
+
+            /* To print the values inside the dictionary
+             * "i" is just what we chose to call it, it could have been kvp as well or whatever we choose
+             * For each row of data "KeyValuePair"...
+             */
+            foreach(KeyValuePair<int, string> i in dictionary)
+            {
+                Console.WriteLine(i);
+            }
+
+            string endMessage = Console.ReadLine();
+
+            Console.WriteLine("Confirmation \"{0}\" received... " +
+                "\nProgram ended", endMessage);
+
+        } 
+
+    }
+}
---
+++ learningFibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/* Fibonacci Sequence
+ * Built using instructor's example
+ */
+
+namespace learningFibonacci
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Calling the Fibonacci Sequence
+            Sequence sqn = new Sequence();
+
+            int[] x = sqn.get();
+            foreach (int i in x)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public class Sequence
+    {
+
+        // Creating an Array of integers to store the Fibonacci values
+        int[] a = new int[102];
+
+        // Generating the Fibonacci values
+        public int[] get()
+        {
+            a[0] = 1;
+            a[1] = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                a[i + 2] = a[i] + a[i + 1];
+            }
+            return a;
+        }
+    }
+
+}
