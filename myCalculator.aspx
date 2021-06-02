819362fd26c8a6b489f0c9e61b873a549bd94bb2
ataahOnGithub <69350396+ataahOnGithub@users.noreply.github.com>
2021-06-01 21:12:37 -0400

    week2Assignment
    Calculator Web Form Assignment (DotNetTraining)

---
+++ myCalculator.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="myCalculator.aspx.cs" Inherits="myCalculator.myCalculator" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <table>
+
+        <style>
+
+        </style>
+
+        <h1>My Personal Calculator</h1>
+        <br />
+
+        <tr>
+            <td colspan="4">
+                <asp:TextBox ID="txtDisplay" Height="30px" runat="server" Width="320px"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td><asp:Button ID="btn7" runat="server" Height="35px" Text="7" Width="80px" OnClick="btn7_Click"/></td>
+            <td><asp:Button ID="btn8" runat="server" Height="35px" Text="8" Width="80px" OnClick="btn8_Click"/></td>
+            <td><asp:Button ID="btn9" runat="server" Height="35px" Text="9" Width="80px" OnClick="btn9_Click"/></td>
+            <td><asp:Button ID="btnDivision" runat="server" Height="35px" Text="/" Width="80px" OnClick="btnDivision_Click"/></td>
+        </tr>
+
+        <tr>
+            <td><asp:Button ID="btn4" runat="server" Height="35px" Text="4" Width="80px" OnClick="btn4_Click"/></td>
+            <td><asp:Button ID="btn5" runat="server" Height="35px" Text="5" Width="80px" OnClick="btn5_Click"/></td>
+            <td><asp:Button ID="btn6" runat="server" Height="35px" Text="6" Width="80px" OnClick="btn6_Click"/></td>
+            <td><asp:Button ID="btnMultiply" runat="server" Height="35px" Text="*" Width="80px" OnClick="btnMultiply_Click"/></td>
+        </tr>
+
+        <tr>
+            <td><asp:Button ID="btn1" runat="server" Height="35px" Text="1" Width="80px" OnClick="btn1_Click"/></td>
+            <td><asp:Button ID="btn2" runat="server" Height="35px" Text="2" Width="80px" OnClick="btn2_Click"/></td>
+            <td><asp:Button ID="btn3" runat="server" Height="35px" Text="3" Width="80px" OnClick="btn3_Click"/></td>
+            <td><asp:Button ID="btnMinus" runat="server" Height="35px" Text="-" Width="80px" OnClick="btnMinus_Click"/></td>
+        </tr>
+
+         <tr>
+            <td><asp:Button ID="btn0" runat="server" Height="35px" Text="0" Width="80px" OnClick="btn0_Click"/></td>
+            <td><asp:Button ID="btnDot" runat="server" Height="35px" Text="." Width="80px" OnClick="btnDot_Click"/></td>
+            <td><asp:Button ID="btnPlus" runat="server" Height="35px" Text="+" Width="80px" OnClick="btnPlus_Click"/></td>
+            <td><asp:Button ID="btnEqual" runat="server" Height="35px" Text="=" Width="80px" OnClick="btnEqual_Click"/></td>
+        </tr>
+
+    </table>
+
+</asp:Content>
---
+++ myCalculator.aspx.cs
@@ -0,0 +1,549 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myCalculator
+{
+    public partial class myCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "1";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "1";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "1";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "1";
+
+                }
+
+            }
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "2";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "2";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "2";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "2";
+
+                }
+
+            }
+
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "3";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "3";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "3";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "3";
+
+                }
+
+            }
+
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "4";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "4";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "4";
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "4";
+
+                }
+
+            }
+
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "5";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "5";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "5";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "5";
+
+                }
+
+            }
+
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+
+            if (Session["op"].ToString() != "")
+            {
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "6";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "6";
+
+                }
+
+            }
+
+            else
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "6";
+
+                }
+                catch
+                {
+
+                    Session["FirstValue"] = "6";
+
+                }
+
+            }
+
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "7";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "7";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "7";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "7";
+
+                }
+
+            }
+
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string op = Session["op"].ToString();
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "8";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "8";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "8";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "8";
+
+                }
+
+            }
+
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+
+            string op = Session["op"].ToString();
+
+            try
+            {
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "9";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "9";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = fv + "9";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "9";
+
+                }
+
+            }
+
+
+        }
+
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+
+            string op = Session["op"].ToString();
+
+            try
+            {
+
+                try
+                {
+
+                    int sv = int.Parse(Session["SecondValue"].ToString());
+                    Session["SecondValue"] = sv + "0";
+
+                }
+
+                catch
+                {
+
+                    Session["SecondValue"] = "0";
+
+                }
+
+            }
+
+            catch
+            {
+
+                try
+                {
+
+                    int fv = int.Parse(Session["FirstValue"].ToString());
+                    Session["FirstValue"] = "0";
+
+                }
+
+                catch
+                {
+
+                    Session["FirstValue"] = "0";
+
+                }
+
+            }
+
+        }
+
+        protected void btnDot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+            Session["op"] = "mult";
+
+        }
+
+        protected void btnPlus_Click(object sender, EventArgs e)
+        {
+
+            Session["op"] = "add";
+
+        }
+
+        protected void btnMinus_Click(object sender, EventArgs e)
+        {
+
+            Session["op"] = "sub";
+
+        }
+
+        protected void btnDivision_Click(object sender, EventArgs e)
+        {
+
+            Session["op"] = "div";
+
+        }
+
+        protected void btnEqual_Click(object sender, EventArgs e)
+        {
+
+            int fv = int.Parse(Session["FirstValue"].ToString());
+            int sv = int.Parse(Session["SecondValue"].ToString());
+            string op = Session["op"].ToString();
+            switch (op)
+            {
+
+
+                case "add":
+                    txtDisplay.Text = (sv + fv).ToString();
+                    break;
+
+                case "mult":
+                    txtDisplay.Text = (sv * fv).ToString();
+                    break;
+
+                case "sub":
+                    txtDisplay.Text = (fv - sv).ToString();
+                    break;
+
+                case "div":
+                    txtDisplay.Text = (fv / sv).ToString();
+                    break;
+
+            }
+
+            Session.Clear();
+
+        }
+
+    }
+
+}
